92799cadddf3905faffc20d137127385ee943435
lescudero <lescudero@runaid.com.ar>
2024-03-17 14:10:16 -0300

    Se agrega el loro

--- src/modules/wsp/wsp.module.ts
+++ src/modules/wsp/wsp.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WspController } from './wsp.controller';
 import { WspService } from './wsp.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [WspController],
   providers: [WspService]
 })
